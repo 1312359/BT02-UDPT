af795b8c99314e1dc36e75b7fee20a14ed424d6c
FT Rinkitori <FT Rinkitori>
2016-04-30 18:51:44 +0700

    Chuc nang login, register, logout, view engine

--- APP/routes/index.js
+++ APP/routes/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var user = require('../model/user');
 var mongoose = require('mongoose');
-router.get('/', function(req, res) {
+//router.get('/', function(req, res) {
 
     //mongoose.model('user').find(name : 'Phan', function(err,user){
     //    res.send(user);
     //});
-    res.render('index');
+    //res.render('index');
+//});
 
+
+router.get('/',function(req,res){
+    res.render('index',{
+        isAuthenticated:req.isAuthenticated(),
+        user:req.user
+    });
 });
-
 module.exports = router;
--- APP/routes/user.js
+++ APP/routes/user.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var user = require('../model/user');
 router.get('/', function(req, res) {
     res.render('user', { title: 'user' });
 });
